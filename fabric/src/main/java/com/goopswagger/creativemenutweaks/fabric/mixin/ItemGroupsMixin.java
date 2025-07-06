c77c273f7470e1768af1586793e3f9fecbd48821
forkiesassds <icanttellyou@memeware.net>
2025-07-06 13:49:22 +0300

    Fix crash on production

---
+++ fabric/src/main/java/com/goopswagger/creativemenutweaks/fabric/mixin/ItemGroupsMixin.java
@@ -0,0 +1,34 @@
+package com.goopswagger.creativemenutweaks.fabric.mixin;
+
+import com.goopswagger.creativemenutweaks.util.ItemGroupUtil;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registry;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+@Mixin(ItemGroups.class)
+public class ItemGroupsMixin {
+    //HACK: Since Architectury Loom is a pile of garbage, and doesn't remap targets for methods within lambdas whatsoever,
+    //  we have to do this ugly mess, where we target the unmapped name rather than the one that our mappings expect.
+    @SuppressWarnings({"UnresolvedMixinReference", "InvalidInjectorMethodSignature", "MixinAnnotationTarget"})
+    @WrapOperation(method = "method_51316", at = @At(value = "INVOKE", target = "Lnet/minecraft/class_2378;iterator()Ljava/util/Iterator;", remap = false))
+    private static Iterator<ItemGroup> addCustomItemGroupsToSearch(Registry<ItemGroup> instance, Operation<Iterator<ItemGroup>> original) {
+        List<ItemGroup> groups = new ArrayList<>();
+        Iterator<ItemGroup> it = original.call(instance);
+
+        while (it.hasNext()) {
+            ItemGroup group = it.next();
+            groups.add(group);
+        }
+
+        List<ItemGroup> modified = ItemGroupUtil.addCustomItemGroups(groups);
+        return modified.iterator();
+    }
+}
--- src/main/java/com/goopswagger/creativemenutweaks/mixin/ItemGroupsMixin.java
+++ src/main/java/com/goopswagger/creativemenutweaks/mixin/ItemGroupsMixin.java
@@ -1,18 +1,13 @@
 package com.goopswagger.creativemenutweaks.mixin;
 
 import com.goopswagger.creativemenutweaks.util.ItemGroupUtil;
-import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
-import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemGroups;
-import net.minecraft.registry.Registry;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
 @Mixin(ItemGroups.class)
@@ -20,19 +15,5 @@ public abstract class ItemGroupsMixin {
     @Inject(method = {"getGroupsToDisplay", "getGroups"}, at = @At(value = "TAIL"), cancellable = true)
     private static void getGroups(CallbackInfoReturnable<List<ItemGroup>> cir) {
         cir.setReturnValue(ItemGroupUtil.addCustomItemGroups(cir.getReturnValue()));
-    }
-
-    @WrapOperation(method = "method_51316", at = @At(value = "INVOKE", target = "Lnet/minecraft/registry/Registry;iterator()Ljava/util/Iterator;"))
-    private static Iterator<ItemGroup> addCustomItemGroupsToSearch(Registry<ItemGroup> instance, Operation<Iterator<ItemGroup>> original) {
-        List<ItemGroup> groups = new ArrayList<>();
-        Iterator<ItemGroup> it = original.call(instance);
-
-        while (it.hasNext()) {
-            ItemGroup group = it.next();
-            groups.add(group);
-        }
-
-        List<ItemGroup> modified = ItemGroupUtil.addCustomItemGroups(groups);
-        return modified.iterator();
     }
 }
